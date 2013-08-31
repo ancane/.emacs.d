b00086b840bed2fea4b190cf863e4c59ce49ec68
Igor Shymko <i.shymko@thenewmotion.com>
2013-08-31 13:50:24 +0300

    powerline customization

--- el-get-init/init-emacs-powerline.el
+++ el-get-init/init-emacs-powerline.el
@@ -2,6 +2,30 @@
 
 (setq powerline-arrow-shape 'arrow)
 
-(custom-set-faces
- '(mode-line ((t (:foreground "#030303" :background "#FF9200" :box nil))))
- '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil)))))
+
+(set-face-attribute 'mode-line nil
+		    :foreground "#030303"
+                    :background "#FFA319"
+                    :box nil)
+
+;; (set-face-attribute 'mode-line nil
+;;                     :background "OliveDrab3"
+;;                     :box nil)
+
+(defpowerline ins-mode (if overwrite-mode "Ovr" "  "))
+
+(setq-default mode-line-format
+              (list "%e"
+                    '(:eval (concat
+                             (powerline-rmw            'left   nil  )
+                             (powerline-buffer-id      'left   nil  powerline-color1  )
+                             (powerline-major-mode     'left        powerline-color1  )
+			     ;; (powerline-minor-modes    'left        powerline-color1  )
+                             (powerline-narrow         'left        powerline-color1  powerline-color2  )
+                             (powerline-vc             'center                        powerline-color2  )
+                             (powerline-make-fill                                     powerline-color2  )
+                             (powerline-row            'right       powerline-color1  powerline-color2  )
+                             (powerline-make-text      ":"          powerline-color1  )
+                             (powerline-column         'right       powerline-color1  )
+                             (powerline-percent        'right  nil  powerline-color1  )
+			     (powerline-ins-mode       'right  nil  )))))
--- init.el
+++ init.el
@@ -10,13 +10,6 @@
  '(safe-local-variable-values (quote ((eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook" (add-hook (quote write-contents-functions) (lambda nil (delete-trailing-whitespace) nil)) (require (quote whitespace)) "Sometimes the mode needs to be toggled off and on." (whitespace-mode 0) (whitespace-mode 1)) (whitespace-line-column . 80) (whitespace-style face trailing lines-tail) (require-final-newline . t))))
  '(projectile-keymap-prefix (kbd "C-c C-p"))
 )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(mode-line ((t (:foreground "#030303" :background "#FF9200" :box nil))))
- '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil)))))
 
 ;; Custom init files
 (add-to-list 'load-path "~/.emacs.d/custom")
