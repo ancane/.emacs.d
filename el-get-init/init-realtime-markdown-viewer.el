891ba7a70293a02c50f1a98eb379aaeeb5646229
Igor Shymko <i.shymko@thenewmotion.com>
2014-09-11 08:47:08 +0300

    mkdown table replace function wip

--- el-get-init/init-realtime-markdown-viewer.el
+++ el-get-init/init-realtime-markdown-viewer.el
@@ -1,1 +1,15 @@
 (require 'realtime-markdown-viewer)
+
+(setq rtmv:lang 'ruby)
+
+(defun ancane-markdown-replace-table-syntax ()
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+
+    (while (re-search-forward "^\\( *-+ *|\\)* *-+$")
+      (message (point))
+      )
+    ))
+
+;; row matching regexp ^[^-|\n]+|\\([^-|\n]*| *\\)*[^-|\n]+$
