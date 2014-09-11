90b4f81141025d320570512bbab7d65106dfd7bc
Igor Shymko <i.shymko@thenewmotion.com>
2014-09-11 14:40:27 +0300

    custom realtime markdown viewer init

--- README.md
+++ README.md
@@ -27,10 +27,3 @@
 ### Modeline: powerline
 
 ### Theme: modified tango-dark
-
-
-
-Table Header 1 | Table Header 2 | Table Header 3
--------------- | -------------- | --------------
-Row 1 col 1 | Row 1 col 2 | Row 1 col 3
-Row 2 col 1 | Row 2 col 2 | Row 2 col 3
--- el-get-init/init-realtime-markdown-viewer.el
+++ el-get-init/init-realtime-markdown-viewer.el
@@ -2,14 +2,61 @@
 
 (setq rtmv:lang 'ruby)
 
+(defun rtmv:send-to-server ()
+  (when realtime-markdown-viewer-mode
+    (let ((oldbuf (current-buffer)))
+      (with-current-buffer (get-buffer-create "*realtime-markdown*")
+        (goto-char (point-min))
+        (push-mark)
+        (goto-char (point-max))
+        (kill-region (mark) (point))
+        (insert-buffer oldbuf)
+        (ancane-markdown-replace-code-syntax)
+        (ancane-markdown-replace-table-syntax)
+        (websocket-send-text rtmv:websocket (buffer-substring-no-properties (point-min) (point-max)))))))
+
+(defun rtmv:init ()
+  (let ((port rtmv:port))
+    (rtmv:webapp-launch port)
+    (sleep-for 1)
+    (rtmv:init-websocket port)
+    (add-hook 'kill-emacs-hook 'rtmv:kill-process)
+    (add-hook 'after-save-hook 'rtmv:send-to-server nil t)))
+
 (defun ancane-markdown-replace-table-syntax ()
-  (interactive)
-  (save-excursion
-    (goto-char (point-min))
-
-    (while (re-search-forward "^\\( *-+ *|\\)* *-+$")
-      (message (point))
+  (goto-char (point-min))
+  (while (re-search-forward "^[^.\n|]+|" (point-max) t)
+    (beginning-of-line)
+    (push-mark)
+    (end-of-paragraph-text)
+    (let* ((text (buffer-substring (mark) (point)))
+           (lines (split-string text "\n")))
+      (kill-region (mark) (point))
+      (insert
+       (concat
+        "<table>\n"
+        (mapconcat '(lambda (line)
+                      (concat
+                       "<tr>"
+                       (mapconcat '(lambda (cell) (concat "<td>" cell "</td>"))
+                                  (split-string line "|")
+                                  "\n")
+                       "</tr>"))
+                   lines
+                   "\n")
+        "\n</table>")
+       )
       )
-    ))
+    )
+  )
 
-;; row matching regexp ^[^-|\n]+|\\([^-|\n]*| *\\)*[^-|\n]+$
+(defun ancane-markdown-replace-code-syntax ()
+  (goto-char (point-min))
+  (while (search-forward "```" (point-max) t)
+    (delete-backward-char 3)
+    (insert "<pre>")
+    (search-forward "```" (point-max) t)
+    (delete-backward-char 3)
+    (insert "</pre>")
+    )
+  )
