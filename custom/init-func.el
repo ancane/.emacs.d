2889f2f4c88ce31e4d71bd0dd86840c199d4afea
Igor Shymko <i.shymko@thenewmotion.com>
2014-10-30 10:51:56 +0200

    purple-haze-theme

--- custom/init-func.el
+++ custom/init-func.el
@@ -13,26 +13,6 @@
       (beginning-of-line)
     (back-to-indentation)))
 
-
-(defun move-line-down ()
-  (interactive)
-  (let ((col (current-column)))
-    (save-excursion
-      (forward-line)
-      (transpose-lines 1))
-    (forward-line)
-    (move-to-column col)))
-
-
-(defun move-line-up ()
-  (interactive)
-  (let ((col (current-column)))
-    (save-excursion
-      (forward-line)
-      (transpose-lines -1))
-    (move-to-column col)))
-
-
 (defun lorem ()
   "Insert a lorem ipsum."
   (interactive)
--- el-get-init/init-move-text.el
+++ el-get-init/init-move-text.el
@@ -1,4 +1,4 @@
 (require 'move-text)
 
-(global-set-key (kbd "C-M-<up>") 'move-text-up)
 (global-set-key (kbd "C-M-<down>") 'move-text-down)
+(global-set-key (kbd "C-M-<up>") 'move-text-up)
