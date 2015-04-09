1e105cdb2beb7146906d13c1d378394439de9e4e
Igor Shymko <igor.shimko@gmail.com>
2015-04-09 11:55:52 +0300

    mdpm hooks

--- custom/init-el-get.el
+++ custom/init-el-get.el
@@ -55,7 +55,6 @@
    scala-outline-popup
    etags-select
    org-mode
-;;   o-blog
    google-translate
    ))
 
--- el-get-init/init-markdown-preview-mode.el
+++ el-get-init/init-markdown-preview-mode.el
@@ -1,1 +1,5 @@
 (require 'markdown-preview-mode)
+
+;(require 'markdown-mode)
+;(add-hook 'markdown-mode-hook (lambda () ( markdown-preview-mode 1 )))
+;(add-hook 'gfm-mode-hook (lambda () ( markdown-preview-mode 1 )))
