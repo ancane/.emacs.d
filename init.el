5d89b6556b6f6390c936064002e57268dd42ad50
Igor Shymko <igor.shimko@gmail.com>
2017-02-11 12:52:38 +0200

    switch to atomone dark theme

--- init.el
+++ init.el
@@ -1,9 +1,11 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("ELPA"      . "http://tromey.com/elpa/"))
 (package-initialize)
+
+;; Custom init files
+(add-to-list 'load-path "~/.emacs.d/custom")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -13,10 +15,10 @@
  '(ansi-color-names-vector
    ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
  '(browse-url-browser-function (quote browse-url-chromium))
- '(custom-enabled-themes (quote (cyberpunk)))
+ '(custom-enabled-themes (quote (atom-one-dark)))
  '(custom-safe-themes
    (quote
-    ("6fc1dd081e496de5ad3e13250c759987e10c71bbf31c102a7dd2b74049d540cb" default)))
+    ("08b8807d23c290c840bbb14614a83878529359eaba1805618b3be7d61b0b0a32" "6fc1dd081e496de5ad3e13250c759987e10c71bbf31c102a7dd2b74049d540cb" default)))
  '(custom-theme-directory "~/.emacs.d/custom/themes")
  '(markdown-command "multimarkdown")
  '(markdown-nested-imenu-heading-index t)
@@ -26,7 +28,7 @@
  '(nav-width 32)
  '(package-selected-packages
    (quote
-    (inflections queue ido-yes-or-no package-lint web-server let-alist)))
+    (atom-one-dark-theme inflections queue ido-yes-or-no package-lint web-server let-alist)))
  '(projectile-keymap-prefix (kbd "C-c C-p"))
  '(restclient-print-curl t)
  '(safe-local-variable-values
@@ -49,17 +51,6 @@
      (whitespace-style face trailing lines-tail)
      (require-final-newline . t)))))
 
-;; Custom init files
-(add-to-list 'load-path "~/.emacs.d/custom")
-
-;; Load custom init files
-(load "init-package.el")
-(load "init-general.el")
-(load "init-el-get.el")
-(load "init-func.el")
-(load "init-ibuffer.el")
-(load "init-keys.el")
-
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -69,3 +60,10 @@
  '(mode-line ((t (:foreground "#030303" :background "#FFA319" :box nil))))
  '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil))))
  '(popup-isearch-match ((t (:inherit font-lock-keyword-face :background "yellow" :foreground "black" :weight bold)))))
+
+;; Load custom init files
+(load "init-general.el")
+(load "init-el-get.el")
+(load "init-func.el")
+(load "init-ibuffer.el")
+(load "init-keys.el")
