680b1a2512ca5a408089cb5ced78e54b7f765f30
Igor Shymko <igor.shimko@gmail.com>
2017-08-20 13:27:29 +0300

    popup-imenu settings

--- el-get-init/init-emacs-popup-imenu.el
+++ el-get-init/init-emacs-popup-imenu.el
@@ -4,4 +4,12 @@
 (setq popup-imenu-position 'point)
 (setq imenu-auto-rescan t)
 
+(setq popup-imenu-style 'indent)
+
 (global-set-key (kbd "C-e") 'popup-imenu)
+
+
+(setq popup-imenu-position 'point)
+(setq popup-imenu-style 'indent)
+(global-set-key (kbd "C-p") 'popup-imenu)
+(define-key popup-isearch-keymap (kbd "C-p") 'popup-isearch-cancel)
